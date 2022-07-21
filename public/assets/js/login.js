cadcaa4edb3684bb675fe7e9485e225b66ffa902
cheddar <nowaczewskijack@gmail.com>
2022-07-21 19:59:54 -0400

    add login

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -2,7 +2,6 @@ function loginHandler(event) {
     event.preventDefault();
     const email = document.querySelector("#email").value.trim();
     const password = document.querySelector("#password").value.trim();
-//   if email and password 
     if(email && password){
         const response = await fetch("/assets/login", {
             method: 'post',
