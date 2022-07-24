b4bfa7ff11d1d8523a5c92e53059c0d615955588
cheddar <nowaczewskijack@gmail.com>
2022-07-24 14:27:39 -0400

    check for issue with route for bug

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,56 @@
-const req = require("express/lib/request");
-const res = require("express/lib/response");
-
+const sequelize = require("sequelize");
+const { Bug, Comment, Upvote, User } = require("../models");
 const router = require("express").Router();
 
+// get all posts for home page
 router.get("/", (req, res) => {
-  console.log("Hello World");
-  res.render("homepage");
+  console.log("<<<<<<<<>>>>>>>>");
+  Bug.findAll({
+    attributes: [
+      "id",
+      "language",
+      "question",
+      "image_file",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM upvote WHERE bug.id = upvote.bug_id)"
+        ),
+        "upvote_count",
+      ],
+    ],
+    order: [
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM upvote WHERE bug.id = upvote.bug_id)"
+        ),
+        "DESC",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "bug_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBugData) => {
+      const bug = dbBugData.map((bug) => bug.get({ plain: true }));
+
+      res.render("homepage", dbBugData[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/login", (req, res) => {
