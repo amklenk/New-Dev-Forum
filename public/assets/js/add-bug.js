61e5c91809d2da367d7141b0f7273f9c6c6dea9b
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-25 12:53:31 -0400

    added file size validation to add-bug.js

--- public/assets/js/add-bug.js
+++ public/assets/js/add-bug.js
@@ -4,24 +4,31 @@ async function bugClickHandler(event) {
 
   const formData = new FormData();
 
-  const language = document.querySelector('#exampleFormControlSelect1').value;
-  const bug_photo = document.querySelector('input[type=file]').files[0];
-  const question = document.querySelector('#exampleFormControlTextarea1').value;
+  const language = document.querySelector("#exampleFormControlSelect1").value;
+  const bug_photo = document.querySelector("input[type=file]").files[0];
+  const question = document.querySelector("#exampleFormControlTextarea1").value;
 
-  formData.append('language', language);
-  formData.append('bug_photo', bug_photo);
-  formData.append('question', question);
+  const fileSize = bug_photo.size / 1024 / 1024;
+  if (fileSize > 2) {
+    alert("File size exceeds 2 MiB");
+  } else {
+    formData.append("bug_photo", bug_photo);
+  }
+
+  formData.append("language", language);
+
+  formData.append("question", question);
 
   const response = await fetch(`/api/bugs`, {
-    method: 'POST',
+    method: "POST",
     body: formData,
   });
 
   if (response.ok) {
-    document.location.replace('/');
+    document.location.replace("/");
   } else {
     alert(response.statusText);
   }
 }
 
-document.querySelector('#submit').addEventListener('click', bugClickHandler);
+document.querySelector("#submit").addEventListener("click", bugClickHandler);
