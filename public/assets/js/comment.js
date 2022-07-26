77872d4548a27dc74c07754d9f53614fdd54c6d9
Amanda Klenk <amandamklenk3@gmail.com>
2022-07-26 14:48:35 -0400

    put helpers in handlebars

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -25,4 +25,4 @@ async function commentFormHandler(event) {
   }
 }
 
-document.querySelector(".comment-form").addEventListener("submit", commentFormHandler);
+document.querySelector("#comment-form").addEventListener("submit", commentFormHandler);
