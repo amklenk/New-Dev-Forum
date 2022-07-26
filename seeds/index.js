a5cb792d09ed22d7c67f34865404fe1a220e0cd0
cheddar <nowaczewskijack@gmail.com>
2022-07-25 20:13:39 -0400

    remove side widgets

--- seeds/index.js
+++ seeds/index.js
@@ -1,25 +1,25 @@
-const seedUsers = require('./user-seeds');
-const seedBugs = require('./bug-seeds');
-const seedComments = require('./comment-seeds');
-const seedUpvotes = require('./upvote-seeds');
+const seedUsers = require("./user-seeds");
+const seedBugs = require("./bug-seeds");
+const seedComments = require("./comment-seeds");
+const seedUpvotes = require("./upvote-seeds");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  console.log("\n----- DATABASE SYNCED -----\n");
 
   await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
+  console.log("\n----- USERS SEEDED -----\n");
 
   await seedBugs();
-  console.log('\n----- BUGS SEEDED -----\n');
+  console.log("\n----- BUGS SEEDED -----\n");
 
   await seedComments();
-  console.log('\n----- COMMENTS SEEDED -----\n');
+  console.log("\n----- COMMENTS SEEDED -----\n");
 
   await seedUpvotes();
-  console.log('\n----- UPVOTES SEEDED -----\n');
+  console.log("\n----- UPVOTES SEEDED -----\n");
 
   process.exit(0);
 };
