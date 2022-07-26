adb61ed6fbc719501da942d9f44d9415b8b1f4fb
glo6al <towers.a@gmail.com>
2022-07-26 12:55:34 -0400

    minor change to fix bug

--- public/assets/js/upvote.js
+++ public/assets/js/upvote.js
@@ -4,7 +4,7 @@ async function upvoteClickHandler(event) {
   const id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
   ];
-  const response = await fetch("/api/bug-routes", {
+  const response = await fetch("/api/posts/upvote", {
     method: "PUT",
     body: JSON.stringify({
       bug_id: id,
